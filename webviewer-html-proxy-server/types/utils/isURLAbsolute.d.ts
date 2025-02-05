ac03f8b21473e7f957537fd8b795ad2565bab291
DavidEGutierrez <david.gutierrez@apryse.com>
2025-02-05 15:59:39 -0500

    Dropped project as it is part of a product that is no longer maintained
    Removed this sample project that was a server complement to a dropped project `webviewer-html-annotate-proxy`.

--- webviewer-html-proxy-server/types/utils/isURLAbsolute.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const isURLAbsolute: (url: string) => boolean;
-export declare const getCorrectHref: (url: string) => string;
--- webviewer-html-proxy-server/types/utils/proxyFailedPage.d.ts
+++
@@ -1,2 +0,0 @@
-declare const getProxyFailedPage: (error?: string) => string;
-export { getProxyFailedPage };
