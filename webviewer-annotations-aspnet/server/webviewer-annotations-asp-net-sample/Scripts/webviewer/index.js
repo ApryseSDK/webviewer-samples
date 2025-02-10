8922beddaeb8a949576c20584c4cff6ce23400ad
Mohammed AbdulRahman <mohammed.abdulrahman@apryse.com>
2025-02-10 10:24:40 +0300

    Make the "Save Annotations" button visible for webviewer-annotations-XXX
    Updated the idex.js

--- webviewer-annotations-aspnet/server/webviewer-annotations-asp-net-sample/Scripts/webviewer/index.js
+++ webviewer-annotations-aspnet/server/webviewer-annotations-asp-net-sample/Scripts/webviewer/index.js
@@ -6,28 +6,33 @@ WebViewer({
   initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo.pdf',
   documentXFDFRetriever: () => loadXfdfString(DOCUMENT_ID)
 }, viewerElement).then(instance => {
-  var annotManager = instance.Core.documentViewer.getAnnotationManager();
-
+  const {annotationManager} = instance.Core;
+  
   // Add a save button on header
-  instance.UI.setHeaderItems(function(header) {
-    header.push({
-      type: 'actionButton',
-      img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
-      onClick: function() {
-        // Save annotations when button is clicked
-        // widgets and links will remain in the document without changing so it isn't necessary to export them
-        annotManager.exportAnnotations({ links: false, widgets: false }).then(function (xfdfString) {
-          saveXfdfString(DOCUMENT_ID, xfdfString)
-          .then(function() {
-            alert('Annotations saved successfully.');
-          })
-          .catch(function () {
-            alert('Annotations not saved successfully.');
-          });
+  const topHeader = instance.UI.getModularHeader('default-top-header');
+  const items = topHeader.getItems();
+  
+  const saveButton = {
+    type: 'customButton',
+    img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+    title: 'Save Annotations',
+    onClick: function() {
+      // Save annotations when button is clicked
+      // widgets and links will remain in the document without changing so it isn't necessary to export them
+      annotationManager.exportAnnotations({ links: false, widgets: false }).then(function (xfdfString) {
+        saveXfdfString(DOCUMENT_ID, xfdfString)
+        .then(function() {
+          alert('Annotations saved successfully.');
+        })
+        .catch(function () {
+          alert('Annotations not saved successfully.');
         });
-      }
-    });
-  });
+      });
+    }
+  };
+
+  items.push(saveButton);
+  topHeader.setItems(items);
 });
 
 // Make a POST request with XFDF string
