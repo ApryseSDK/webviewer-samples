dbd23fd44f8379a4f066a896bff5c37b0d4279bd
Hussam Barouqa <hussam.barouqa@apryse.com>
2025-02-17 12:20:11 +0300

    Updated Dependencies and Linting
    - Committed changed done in: https://github.com/ApryseSDK/webviewer-powerapps-component-sample/pull/2
    - Updated `@pdftron/webviewer` to 11.2.0
    - Updated `pcf-scripts` and `pcf-start` to 1.39.3 which now requires the use of `eslint.config.mjs` instead of `.eslintrc.json`
    - Added `eslint.config.mjs` from the basic Power Component template and added the react plugin.
    - Modified `WebViewerControl\index.ts` to resolve the following linting errors:
    ```
    [1] [pcf-1065] [Error] ESLint validation error:
    [1]    7:5   error  Type string trivially inferred from a string literal, remove type annotation  @typescript-eslint/no-inferrable-types
    [1]   16:5   error  Unexpected empty constructor                                                  @typescript-eslint/no-empty-function
    [1]   51:13  error  Unexpected aliasing of 'this' to local variable                               @typescript-eslint/no-this-alias
    [1]   51:13  error  'that' is never reassigned. Use 'const' instead                               prefer-const
    [1]   52:40  error  Unexpected any. Specify a different type                                      @typescript-eslint/no-explicit-any
    [1]   98:13  error  'result' is never reassigned. Use 'const' instead                             prefer-const
    ```

--- webviewer-powerapps-component/README.md
+++ webviewer-powerapps-component/README.md
@@ -1,15 +1,14 @@
 # WebViewer - Powerapps component sample
 
-[WebViewer](https://docs.apryse.com/web/guides/get-started) is a powerful JavaScript-based PDF Library that is part of the [Apryse SDK](https://apryse.com/). It provides a slick out-of-the-box responsive UI that enables you to view, annotate and manipulate PDFs and other document types inside any web project.
+[WebViewer](https://docs.apryse.com/documentation/web/) is a powerful JavaScript-based PDF Library that is part of the [Apryse SDK](https://apryse.com/). It provides a slick out-of-the-box responsive UI that interacts with the core library to view, annotate and manipulate PDFs that can be embedded into web projects.
 
-- [WebViewer Documentation](https://docs.apryse.com/web/guides/get-started)
-- [WebViewer Demo](https://showcase.apryse.com/)
+![WebViewer UI](https://www.pdftron.com/downloads/pl/webviewer-ui.png)
 
-This sample is specifically designed for any users interested in integrating WebViewer into a [Powerapps](https://learn.microsoft.com/en-us/power-apps/maker/canvas-apps/create-component) component project. 
+This repo is specifically designed for any users interested in integrating WebViewer into a [Powerapps](https://learn.microsoft.com/en-us/power-apps/maker/canvas-apps/create-component) component project.
 
-## Get your trial key
+## Demo
 
-A license key is required to run WebViewer. You can obtain a trial key in our [get started guides](https://docs.apryse.com/web/guides/get-started), or by signing-up on our [developer portal](https://dev.apryse.com/).
+You can explore all of the functionality in our [showcase](https://showcase.apryse.com/).
 
 ## Initial setup
 
@@ -22,8 +21,8 @@ Before you begin, make sure your development environment includes:
 ## Install
 
 ```
-git clone --depth=1 https://github.com/ApryseSDK/webviewer-samples.git
-cd webviewer-samples/webviewer-powerapps-component
+git clone https://github.com/ApryseSDK/webviewer-powerapps-component-sample.git
+cd webviewer-powerapps-component-sample
 npm install
 ```
 
@@ -38,4 +37,23 @@ After the app starts, you will be able to see WebViewer running on `localhost:81
 ![WebViewer Control within PowerApps Component](powerapps-component-webviewer-control.png)
 
 
+
 Visit Apryse's [WebViewer](https://docs.apryse.com/documentation/web/) page to see what else you can do with the WebViewer.
+
+## WebViewer APIs
+
+* [Webviewer API documentation](https://docs.apryse.com/api/web/WebViewerInstance.html)
+* [webviewer-powerapps-component-sample](https://github.com/ApryseSDK/webviewer-powerapps-component-sample)
+
+## Showcase
+
+Refer to a running sample on Apryse SDK [showcase page](https://showcase.apryse.com/).
+
+## Contributing
+
+Any submission to this repo is governed by these [guidelines](/CONTRIBUTING.md).
+
+
+## License
+
+For licensing, refer to [License](LICENSE).
--- webviewer-powerapps-component/WebViewerControl/index.ts
+++ webviewer-powerapps-component/WebViewerControl/index.ts
@@ -1,19 +1,16 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import WebViewer from '@pdftron/webviewer'
 
 export class WebViewerControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
     private _notifyOutputChanged: () => void;
-    private pdfURI: string = "";
+    private pdfURI = "";
     private iframeWindow: Window;
     private _context: ComponentFramework.Context<IInputs>;
     private _container: HTMLDivElement;
 
-    /**
-     * Empty constructor.
-     */
-    constructor()
-    {
+    constructor() {
+        // Empty constructor
     }
 
     /**
@@ -24,8 +21,7 @@ export class WebViewerControl implements ComponentFramework.StandardControl<IInp
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
      */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
         // Add control initialization code
         this._notifyOutputChanged = notifyOutputChanged;
         this._context = context;
@@ -40,26 +36,23 @@ export class WebViewerControl implements ComponentFramework.StandardControl<IInp
             this.iframeWindow = viewerElement.querySelector('iframe')!.contentWindow!;
         })
 
-        WebViewer({
+        WebViewer.Iframe({
             path: 'http://localhost:3000/lib',
             config: 'http://localhost:3000/config.js',
             initialDoc: context.parameters.doc.raw!,
-        },viewerElement)
+        }, viewerElement)
 
-        window.addEventListener("message", receiveMessage, false);
-
-        let that = this;
-        function receiveMessage(event: any) {
+        window.addEventListener("message", (event: MessageEvent) => {
             if (event.isTrusted && typeof event.data === 'object') {
                 switch (event.data.type) {
-                case 'SAVE_DOCUMENT':
-                    that.handleDocSave(event.data.payload.file);
-                    break;
-                default:
-                    break;
+                    case 'SAVE_DOCUMENT':
+                        this.handleDocSave(event.data.payload.file);
+                        break;
+                    default:
+                        break;
                 }
             }
-        }
+        }, false);
     }
 
     private handleDocOpen(docUrl: string): void {
@@ -78,25 +71,22 @@ export class WebViewerControl implements ComponentFramework.StandardControl<IInp
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
      */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
         this._container.style.height = context.parameters.viewerheight.raw + "px";
         this._container.style.width = context.parameters.viewerwidth.raw + "px";
-        if(context.updatedProperties.indexOf("doc")> -1)
-        {
+        if (context.updatedProperties.indexOf("doc") > -1) {
             this._context = context;
             this.handleDocOpen(context.parameters.doc.raw!)
-        }    
+        }
     }
 
     /**
      * It is called by the framework prior to a control receiving new data.
      * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
      */
-    public getOutputs(): IOutputs
-    {
-        let result = {
-            pdfdoc : this.pdfURI
+    public getOutputs(): IOutputs {
+        const result = {
+            pdfdoc: this.pdfURI
         };
         return result;
     }
@@ -105,8 +95,7 @@ export class WebViewerControl implements ComponentFramework.StandardControl<IInp
      * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
      * i.e. cancelling any pending remote calls, removing listeners, etc.
      */
-    public destroy(): void
-    {
+    public destroy(): void {
         // Add code to cleanup control if necessary
     }
 }
