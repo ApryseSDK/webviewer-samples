ea91d3544d9efb467f8431e2622d53059e2735a7
Mohammed AbdulRahman <mohammed.abdulrahman@apryse.com>
2026-06-11 12:31:20 +0300

    webviewer-user-bookmarks-nodejs: Replaced e.detail payload by bookmarks argument

--- webviewer-user-bookmarks-nodejs/client/index.js
+++ webviewer-user-bookmarks-nodejs/client/index.js
@@ -7,8 +7,7 @@ WebViewer({
   // ui: 'legacy',
 }, viewerElement).then((instance) => {
 
-  instance.UI.addEventListener('userBookmarksChanged', (detail) => {
-    const bookmarks = { detail };
+  instance.UI.addEventListener('userBookmarksChanged', (bookmarks) => {
     const bookmarksString = JSON.stringify(bookmarks);
     saveBookmarksString(DOCUMENT_ID, bookmarksString).then(() => {
       console.log('Bookmarks saved successfully.');
