000ec1de23edd2b414f9ff293d27a3d1910eb020
Mohammed AbdulRahman <mohammed.abdulrahman@apryse.com>
2026-07-06 12:33:18 +0300

    Adding Snipping and Flatten Annotations tools button
    Replacing the old way of having custom tool buttons that is ineffective in webviewer v12

--- webviewer-barcode/src/components/WebViewerPDFTron/index.js
+++ webviewer-barcode/src/components/WebViewerPDFTron/index.js
@@ -59,33 +59,32 @@ const WebViewerPDFTron = () => {
       });
 
       // Add tool button in header
-      instance.UI.getModularHeaderList((header) => {
-        header
-          .getHeader('toolbarGroup-Edit')
-          .get('cropToolButton')
-          .insertAfter({
-            type: 'toolButton',
-            toolName: 'SnipTool',
-          })
-          .insertAfter({
-            type: 'actionButton',
-            img:
-              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
-            onClick: async () => {
-              // flatten annotations
-              const annots = await annotationManager.exportAnnotations();
-              const fdf_doc = await PDFNet.FDFDoc.createFromXFDF(annots);
-              const doc = await documentViewer.getDocument().getPDFDoc();
-              await doc.fdfUpdate(fdf_doc);
-              await doc.flattenAnnotations();
-              annotationManager.deleteAnnotations(annotationManager.getAnnotationsList());
-              documentViewer.refreshAll();
-              documentViewer.updateView();
-              documentViewer.getDocument().refreshTextData();
-            },
-            title: 'Flatten Annotations',
-          });
-      });
+      const editGroup = instance.UI.getGroupedItems('editGroupedItems');
+      editGroup.setItems([
+        // snipping tool button
+        {
+          type: 'toolButton',
+          toolName: 'SnipTool'
+        },
+        // custom button to flatten annotations
+        new instance.UI.Components.CustomButton({
+          dataElement: 'customButton',
+          className: 'custom-button-class',
+          onClick: async () => {
+            // flatten annotations
+            const annots = await annotationManager.exportAnnotations();
+            const fdf_doc = await PDFNet.FDFDoc.createFromXFDF(annots);
+            const doc = await documentViewer.getDocument().getPDFDoc();
+            await doc.fdfUpdate(fdf_doc);
+            await doc.flattenAnnotations();
+            annotationManager.deleteAnnotations(annotationManager.getAnnotationsList());
+            documentViewer.refreshAll();
+            documentViewer.updateView();
+            documentViewer.getDocument().refreshTextData();
+          },
+          img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+          title: 'Flatten Annotations',
+        })]);
 
       customSnipTool.addEventListener('annotationAdded', annotation => {
         const pageIndex = annotation.PageNumber;
