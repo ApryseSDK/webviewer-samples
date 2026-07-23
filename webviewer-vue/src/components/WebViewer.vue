dae38ac8294e9197fe54d79cbe650be1617d0e08
AminDodin-Apryse <amin.dodin@apryse.com>
2026-07-23 16:34:31 +0300

    Setting license key
    Added code to use import.meta.env to inject the value of VITE_DEMO_KEY from the .env file instead of 'your_license_key' in src\components\WebViewer.vue
    This is the standard method  for Vue for injecting environment variables.
    Updated README accordingly.

--- webviewer-vue/src/components/WebViewer.vue
+++ webviewer-vue/src/components/WebViewer.vue
@@ -3,39 +3,42 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import WebViewer from '@pdftron/webviewer';
+import { ref, onMounted } from "vue";
+import WebViewer from "@pdftron/webviewer";
+
+const licenseKey = import.meta.env.VITE_DEMO_KEY;
 
 export default {
-  name: 'WebViewer',
+  name: "WebViewer",
   props: { initialDoc: { type: String } },
   setup(props) {
     const viewer = ref(null);
     onMounted(() => {
-      WebViewer({ 
-        path: '/lib/webviewer', 
-        initialDoc: props.initialDoc, 
-        licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
-      }, viewer.value).then(
-        (instance) => {
-          const { documentViewer, annotationManager, Annotations } =
-            instance.Core;
+      WebViewer(
+        {
+          path: "/lib/webviewer",
+          initialDoc: props.initialDoc,
+          licenseKey,
+        },
+        viewer.value,
+      ).then((instance) => {
+        const { documentViewer, annotationManager, Annotations } =
+          instance.Core;
 
-          documentViewer.addEventListener('documentLoaded', () => {
-            const rectangleAnnot = new Annotations.RectangleAnnotation({
-              PageNumber: 1,
-              // values are in page coordinates with (0, 0) in the top left
-              X: 100,
-              Y: 150,
-              Width: 200,
-              Height: 50,
-              Author: annotationManager.getCurrentUser(),
-            });
-            annotationManager.addAnnotation(rectangleAnnot);
-            annotationManager.redrawAnnotation(rectangleAnnot);
+        documentViewer.addEventListener("documentLoaded", () => {
+          const rectangleAnnot = new Annotations.RectangleAnnotation({
+            PageNumber: 1,
+            // values are in page coordinates with (0, 0) in the top left
+            X: 100,
+            Y: 150,
+            Width: 200,
+            Height: 50,
+            Author: annotationManager.getCurrentUser(),
           });
-        }
-      );
+          annotationManager.addAnnotation(rectangleAnnot);
+          annotationManager.redrawAnnotation(rectangleAnnot);
+        });
+      });
     });
     return {
       viewer,
